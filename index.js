53ee7616f66492869a92f7a6db85e86f0b4878b2
hasnatsajid965 <86589885+hasnatsajid@users.noreply.github.com>
2022-07-16 11:32:35 +0500

    added routes, requests and responses

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const http = require('http');
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  const items = req.url.split('/');
+  //   res.writeHead(200, {
+  //     'Content-Type': 'application/json',
+  //   });
+  if (items[1] === 'friends') {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+
+    res.end(
+      JSON.stringify({
+        id: 1,
+        name: 'Hasnat sajid',
+      })
+    );
+  } else if (items[1] === 'messages') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>list item 1</li>');
+    res.write('<li>list item 2</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end('');
+  }
 });
 
 server.listen(PORT, () => {
